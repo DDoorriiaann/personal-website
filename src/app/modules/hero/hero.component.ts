7ad85318e5aca49f72f2d541d000666b0a651f34
Dorian Guet <dorian.guet@gmail.com>
2021-09-07 22:57:24 +0200

    [FEAT] #Hero: English and French languages are selectable

--- src/app/modules/hero/hero.component.ts
+++ src/app/modules/hero/hero.component.ts
@@ -1,11 +1,4 @@
-import { typeWithParameters } from '@angular/compiler/src/render3/util';
-import {
-  Component,
-  AfterViewInit,
-  ElementRef,
-  ViewChild,
-  OnInit,
-} from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -14,72 +7,106 @@ import {
 })
 export class HeroComponent implements AfterViewInit {
   @ViewChild('jobTitle') jobTitle!: ElementRef;
-
+  language = 'English';
   firstJobTitle = 'Front-End';
   secondJobTitle = 'Back-End';
   thirdJobTitle = 'FullStack';
+  animCount = 0;
   i = 0;
   constructor() {}
   ngAfterViewInit(): void {
     this.write1();
+    if (localStorage.getItem('language')) {
+      this.language = localStorage.getItem('language')!;
+    }
+    if (localStorage.getItem('animCount')) {
+      this.animCount = parseInt(localStorage.getItem('animCount')!, 10);
+    }
   }
-
+  //////////// Typewriter effect
   write1() {
     let speed = Math.random() * 90 + 50;
-    if (this.i < this.firstJobTitle.length) {
+    if (this.i < this.firstJobTitle.length && this.animCount == 0) {
       this.jobTitle.nativeElement.innerHTML += this.firstJobTitle.charAt(
         this.i
       );
       this.i++;
-      console.log('write1');
-      console.log(this.i);
       setTimeout(() => this.write1(), speed);
-    } else if (this.i == this.firstJobTitle.length) {
+    } else if (this.i == this.firstJobTitle.length && this.animCount == 0) {
       this.delete1();
+    } else if (this.animCount == 1) {
+      this.jobTitle.nativeElement.innerHTML = 'FullStack';
     }
   }
   delete1() {
     let speed = Math.random() * 80 + 40;
-    if (this.i >= 1) {
-      this.jobTitle.nativeElement.innerHTML =
-        this.jobTitle.nativeElement.innerHTML.slice(0, -1);
-      this.i--;
-      setTimeout(() => this.delete1(), speed);
-    } else if (this.i == 0) {
-      this.write2();
+    if (this.animCount == 0) {
+      if (this.i >= 1) {
+        this.jobTitle.nativeElement.innerHTML =
+          this.jobTitle.nativeElement.innerHTML.slice(0, -1);
+        this.i--;
+        setTimeout(() => this.delete1(), speed);
+      } else if (this.i == 0) {
+        this.write2();
+      }
+    } else {
+      this.jobTitle.nativeElement.innerHTML = 'FullStack';
     }
   }
   write2() {
     let speed = Math.random() * 90 + 30;
-    if (this.i < this.secondJobTitle.length) {
-      this.jobTitle.nativeElement.innerHTML += this.secondJobTitle.charAt(
-        this.i
-      );
-      this.i++;
-      setTimeout(() => this.write2(), speed);
-    } else if (this.i == this.secondJobTitle.length) {
-      this.delete2();
+    if (this.animCount == 0) {
+      if (this.i < this.secondJobTitle.length) {
+        this.jobTitle.nativeElement.innerHTML += this.secondJobTitle.charAt(
+          this.i
+        );
+        this.i++;
+        setTimeout(() => this.write2(), speed);
+      } else if (this.i == this.secondJobTitle.length) {
+        this.delete2();
+      }
+    } else {
+      this.jobTitle.nativeElement.innerHTML = 'FullStack';
     }
   }
   delete2() {
     let speed = Math.random() * 50 + 20;
-    if (this.i > 0) {
-      this.jobTitle.nativeElement.innerHTML =
-        this.jobTitle.nativeElement.innerHTML.slice(0, -1);
-      this.i--;
-      setTimeout(() => this.delete2(), speed);
-    } else if (this.i == 0) {
-      this.write3();
+    if (this.animCount == 0) {
+      if (this.i > 0) {
+        this.jobTitle.nativeElement.innerHTML =
+          this.jobTitle.nativeElement.innerHTML.slice(0, -1);
+        this.i--;
+        setTimeout(() => this.delete2(), speed);
+      } else if (this.i == 0) {
+        this.write3();
+      }
+    } else {
+      this.jobTitle.nativeElement.innerHTML = 'FullStack';
     }
   }
   write3() {
     let speed = Math.random() * 50;
-    if (this.i < this.thirdJobTitle.length) {
-      this.jobTitle.nativeElement.innerHTML += this.thirdJobTitle.charAt(
-        this.i
-      );
-      this.i++;
-      setTimeout(() => this.write3(), speed);
+    if (this.animCount == 0) {
+      if (this.i < this.thirdJobTitle.length) {
+        this.jobTitle.nativeElement.innerHTML += this.thirdJobTitle.charAt(
+          this.i
+        );
+        this.i++;
+        setTimeout(() => this.write3(), speed);
+      }
+    } else {
+      this.jobTitle.nativeElement.innerHTML = 'FullStack';
     }
+  }
+  stopAnim() {
+    this.animCount = 1;
+  }
+
+  //////////// Language toggle
+
+  toggleLanguage(updatedLanguage: string) {
+    this.language = updatedLanguage;
+    localStorage.setItem('language', updatedLanguage);
+    localStorage.setItem('animCount', '0');
   }
 }
